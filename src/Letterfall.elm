190d9e54647eda4c88e14c1fd615b14b7cc10234
Martin Heuschober <epsilonhalbe@gmail.com>
2017-04-22 22:02:31 +0200

    upgraded to elm v0.18
    minified letterfall.js with `java -jar closure-compiler-v20170409.jar --compilation_level SIMPLE_OPTIMIZATIONS --js resources/js/letterfall.js --js_output_file resources/js/letterfall.min.js`
    - relevant upgrade was to remove Html.App in the import section and change App.program to Html.program
    - tidied up code (removed parens)

--- src/Letterfall.elm
+++ src/Letterfall.elm
@@ -1,7 +1,6 @@
 port module Letterfall exposing (..)
 
-import Html exposing (Html)
-import Html.App as App
+import Html exposing (..)
 import Html.Events exposing (..)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
@@ -11,27 +10,21 @@ import Array
 import Random.Array
 import Random.Int
 
-main =
-  App.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
-
+main = Html.program
+     { init          = init
+     , view          = view
+     , update        = update
+     , subscriptions = subscriptions }
 
 -- MODEL
 
-type alias Model =
-  { letters : List String
-  }
+type alias Model = {letters : List String}
 
+alphabet : List String
 alphabet = String.split "" "abcdefghijklmnopqrstuvwxyz"
 
-
-init: (Model, Cmd Msg)
-init =
-  (Model  alphabet,  d3Update alphabet)
+init : (Model, Cmd Msg)
+init = (Model  alphabet,  d3Update alphabet)
 
 -- UPDATE
 
@@ -39,64 +32,30 @@ type Msg = Shuffle  | ReplaceCards (Int, Array.Array String)
 
 port d3Update : List String -> Cmd msg
 
-
-
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-  case msg of
-    Shuffle ->
-        (model, Random.generate ReplaceCards
-             (Random.pair
-              (Random.int 1 25)
-              (Random.Array.shuffle
-               (Array.fromList alphabet))))
-
-    ReplaceCards (len,chars) ->
-        let newalpha = (Array.toList (Array.slice 0 len chars))
-        in
-            (Model newalpha, d3Update  newalpha)
-
+update msg model = case msg of
+  Shuffle ->
+      let shufleBet = (Random.Array.shuffle <| Array.fromList alphabet)
+       in (model, Random.generate ReplaceCards <| Random.pair (Random.int 1 25) shufleBet)
+  ReplaceCards (len, chars) ->
+      let newAlpha = Array.toList <| Array.slice 0 len chars
+       in (Model newAlpha, d3Update newAlpha)
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
-
-
+subscriptions _ = Sub.none
 
 -- VIEW
-
 -- am going to pass this off to D3 soonish
 
 view : Model -> Html Msg
 view model =
   Html.div []
-    [ Html.h1 [] [ Html.text (toString model.letters) ]
-    ,Svg.svg
-        [ width "960", height "120", viewBox "0 0 960 120"]
-        [ g [class "letters", transform "translate(32,60)"][]
-        ]
+    [ Html.h1 [] [ Html.text <| toString model.letters ]
+    , Svg.svg [ width "960", height "120", viewBox "0 0 960 120"]
+              [ g [class "letters", transform "translate(32,60)"] [] ]
 
-    , Html.button [ onClick Shuffle ] [ text "Roll" ]
+    , Html.button [ onClick Shuffle ] [ Html.text "Roll" ]
     ]
 
-
-svgletters : List String -> List (Svg msg)
-svgletters letters =
-    List.indexedMap svgletter letters
-
-
-svglettersD3 : List String -> List (Svg msg)
-svglettersD3 letters =
-    List.indexedMap svgletter letters
-
-
-
-svgletter : number -> String -> Svg msg
-svgletter i letter =
-    let
-        xpos = (toString (i * 32))
-    in
-        text' [x xpos , y "60", fontSize "55", fill "blue" ]
-            [Svg.text letter]
